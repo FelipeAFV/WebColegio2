24a226b6194b2b297443bcb9794aa8f59a680336
unknown <samuelborraccini@hotmail.com>
2020-11-11 23:12:04 -0300

    actualizacion

---
+++ src/main/java/modelo/ProfesorDTO.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author samuel
+ */
+public class ProfesorDTO extends User{
+    
+    
+    
+    private String name;
+    private String last_name;
+    private String email;
+    private String specialist;
+    
+    public ProfesorDTO(){
+        
+    }
+
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialist() {
+        return specialist;
+    }
+
+    public void setSpecialist(String specialist) {
+        this.specialist = specialist;
+    }
+    
+    
+}
---
+++ src/main/webapp/VistaProfesor.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : VistaProfesor
+    Created on : 11-11-2020, 23:05:50
+    Author     : samuel
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
